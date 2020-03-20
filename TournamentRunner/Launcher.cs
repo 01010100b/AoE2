5e83ae6fe932a500d297671d46ea28e0cf5041da
01010100b <caveman1917@hotmail.com>
2020-03-20 22:27:46 +0100

    add game runner

---
+++ TournamentRunner/Launcher.cs
@@ -0,0 +1,33 @@
+﻿using Reloaded.Injector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentRunner
+{
+    class Launcher
+    {
+        public static void Launch()
+        {
+            var dll = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "aoc-auto-game.dll");
+            var aoc_name = "age2_x1.5.exe";
+            var exe = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), @"Microsoft Games\Age of Empires ii\Age2_x1\" + aoc_name);
+
+            foreach (var p in Process.GetProcessesByName(aoc_name))
+            {
+                p.Kill();
+            }
+
+            var process = Process.Start(exe);
+            
+            using (var injector = new Injector(process))
+            {
+                injector.Inject(dll);
+            }
+        }
+    }
+}
