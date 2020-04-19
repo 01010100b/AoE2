caff98c965c4704d8b4b3ce4abbb0b566025eed3
01010100b <caveman1917@hotmail.com>
2020-04-19 22:30:45 +0200

    m

--- ScriptVM/Block.cs
+++ ScriptVM/Block.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ScriptVM
 {
-    abstract class Block
+    abstract class Block : Statement
     {
         public readonly List<Variable> LocalVariables;
+        public readonly List<Statement> Statements;
     }
 }
--- ScriptVM/Function.cs
+++ ScriptVM/Function.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace ScriptVM
 {
-    class Function : Block
+    class Function
     {
         public readonly VirtualType ReturnType;
         public readonly List<Variable> Parameters;
+        public readonly Block Code;
     }
 }
