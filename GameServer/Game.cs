6e5ee3e92ffd7397ab9b3bca8373a8825fe1e305
01010100b <caveman1917@hotmail.com>
2020-06-27 20:28:11 +0200

    m

---
+++ GameServer/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer
+{
+    class Game
+    {
+    }
+}
---
+++ GameServer/Runner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer
+{
+    class Runner
+    {
+        public readonly string Exe;
+        public readonly int Speed;
+        public readonly string AiFolder;
+        public readonly string RecFolder;
+
+        public void Startup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameResult Run(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
